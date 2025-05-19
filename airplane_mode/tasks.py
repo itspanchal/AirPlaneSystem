b390c4fef8b4996c6941b877b220040aa595efcb
itspanchal <komal612412@gmail.com>
2025-05-19 17:07:22 +0530

    fix the issues

--- airplane_mode/tasks.py
+++ airplane_mode/tasks.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe.utils import getdate
 
-@frappe.background_job()
+
 def update_ticket_gates(flight, new_gate_number):
     airline_tickets = frappe.get_all("Airline Ticket", filters={"flight": flight}, fields=["name"])
     for ticket in airline_tickets:
@@ -13,16 +13,17 @@ def send_rent_reminders():
     if not settings.enable_rent_reminders:
         return
 
-	current_month = getdate().strftime("%B %Y")
+    current_month = getdate().strftime("%B %Y")
     shops = frappe.get_all("Airport Shop", fields=["name", "tenant", "rent_amount"])
     for shop in shops:
         tenant = frappe.get_doc("Tenant", shop.tenant)
         if tenant.email:
             frappe.sendmail(
                 recipients=[tenant.email],
-                subject= f"Rent Due Reminder for {current_month}",
+                subject=f"Rent Due Reminder for {current_month}",
                 message=f"Dear {tenant.tenant_name}, your rent of ₹{shop.rent_amount} for Shop {shop.name} is due."
             )
+
 
 def update_airport_shop_stats():
     airports = frappe.get_all("Airport", fields=["name"])
